8eb62e2a6eb6f100591867ec445ccc04e19032ea
Ernesto Elsäßer <ernesto@Ernestos-MacBook-Air.local>
2013-11-28 16:53:43 +0100

    sigmoidal ratio bar fix 2

--- static/js/session.js
+++ static/js/session.js
@@ -453,10 +453,11 @@ function Session(ui,color1,color2) {
 
 	this.updateRatioBar = function() {
 
-		//var ratio = this.player[1].wins / (this.player[1].wins + this.player[2].wins) * 100;
-		var d = this.player[1].wins - this.player[2].wins;
-		var w = 100 / (1 + Math.pow(Math.E, (-0.3 * d)));
-		this.ui.ratiocol1.style.width = w + "%";
+		var v1 = this.player[1].wins + 3;
+		var v2 = this.player[2].wins + 3;
+		//var d = this.player[1].wins - this.player[2].wins;
+		//var w = 100 / (1 + Math.pow(Math.E, (-0.3 * d)));
+		this.ui.ratiocol1.style.width = (( 100 * v1 )/( v1 + v2 ) ) + "%";
 
 	}
 	
